d2b983e42cc76f7632b25ef985f1190179d02845
xiannv <1511302699@qq.com>
2019-10-12 19:42:47 +0800

    d03

---
+++ gmall-api/src/main/java/com/atguigu/gmall/service/AttrService.java
@@ -0,0 +1,9 @@
+package com.atguigu.gmall.service;
+
+import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+
+import java.util.List;
+
+public interface AttrService {
+ public List<PmsBaseAttrInfo> attrInfoList(String catalog3Id);
+}
