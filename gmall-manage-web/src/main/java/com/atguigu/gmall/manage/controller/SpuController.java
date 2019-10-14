55c5b0953616ffd1be74aeaf70ed6e99a64c8426
xiannv <1511302699@qq.com>
2019-10-14 21:11:02 +0800

    d04剩余

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
@@ -4,9 +4,8 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsProductInfo;
 import com.atguigu.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -23,5 +22,22 @@ public class SpuController {
 
   List<PmsProductInfo> pmsProductInfos= spuService.spuList(catalog3Id);
   return pmsProductInfos;
+ }
+
+ @RequestMapping("saveSpuInfo")
+ @ResponseBody
+ public String saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo){
+  //将图片或者音频上传到分布式的文件存储系统
+
+  //将图片的存储路径返回给页面
+  String imgUrl="";
+  return "success";
+ }
+
+ @RequestMapping("fileUpload")
+ @ResponseBody
+ public String fileUpload(@RequestParam("file") MultipartFile multipartFile){
+
+  return "success";
  }
 }
