4fbfbc71cb222c6e264231af17c6bafe141f4101
xiannv <1511302699@qq.com>
2019-10-17 19:59:23 +0800

    d06 100%

--- gmall-api/src/main/java/com/atguigu/gmall/service/SpuService.java
+++ gmall-api/src/main/java/com/atguigu/gmall/service/SpuService.java
@@ -7,10 +7,14 @@ import com.atguigu.gmall.bean.PmsProductSaleAttr;
 import java.util.List;
 
 public interface SpuService {
+
  public List<PmsProductInfo> spuList(String catalog3Id);
+
  public List<PmsProductSaleAttr> spuSaleAttrList(String spuId);
 
  void saveSpuInfo(PmsProductInfo pmsProductInfo);
 
  List<PmsProductImage> spuImageList(String spuId);
+
+ List<PmsProductSaleAttr> spuSaleAttrListCheckBySku(String productId,String skuId);
 }
--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
@@ -52,4 +52,19 @@ public class SkuServiceImpl implements SkuService {
    pmsSkuImageMapper.insertSelective(pmsSkuImage);
   }
  }
+
+ @Override
+ public PmsSkuInfo getSkuById(String skuId) {
+  //sku商品对象
+  PmsSkuInfo pmsSkuInfo=new PmsSkuInfo();
+  pmsSkuInfo.setId(skuId);
+
+  PmsSkuInfo skuInfo=pmsSkuInfoMapper.selectOne(pmsSkuInfo);
+  //sku图片集合
+  PmsSkuImage pmsSkuImage=new PmsSkuImage();
+  pmsSkuImage.setSkuId(skuId);
+  List<PmsSkuImage> pmsSkuImages=pmsSkuImageMapper.select(pmsSkuImage);
+  skuInfo.setSkuImageList(pmsSkuImages);
+  return skuInfo;
+ }
 }
